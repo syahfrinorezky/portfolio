b480157643c68d2cb052dc7f2dfda98aff6e9a01
syahfrinorezky <syahfrinorezky@gmail.com>
2025-08-23 23:04:47 +0700

    feat: add home page with hero section

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+/** @format */
+
+import React from "react";
+import Hero from "./sections/Hero";
+
+export const metadata = {
+  title: "RR - Home",
+  icons: {
+    icon: "/images/logo/yellowlogo.png",
+  },
+};
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div>
+      <Hero />
+    </div>
+  );
 }
 
-export default Home
+export default Home;
